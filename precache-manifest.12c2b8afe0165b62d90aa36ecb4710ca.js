64bfdb8d3fd5f9212418c9d24b552ff88f83058f
mohit1508 <mohitkhandelwal1997@yahoo.com>
2021-12-25 18:53:03 +0530

    Updates

---
+++ precache-manifest.12c2b8afe0165b62d90aa36ecb4710ca.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "8918ec1f099a015e58c9",
+    "url": "/Robofriends/static/css/main.9daf0f5b.chunk.css"
+  },
+  {
+    "revision": "8918ec1f099a015e58c9",
+    "url": "/Robofriends/static/js/main.8918ec1f.chunk.js"
+  },
+  {
+    "revision": "0bf54a1201dab9a9b136",
+    "url": "/Robofriends/static/js/1.0bf54a12.chunk.js"
+  },
+  {
+    "revision": "c1439accfed68e20cd09",
+    "url": "/Robofriends/static/css/2.6eade045.chunk.css"
+  },
+  {
+    "revision": "c1439accfed68e20cd09",
+    "url": "/Robofriends/static/js/2.c1439acc.chunk.js"
+  },
+  {
+    "revision": "5ba37b3764872da53460",
+    "url": "/Robofriends/static/js/runtime~main.5ba37b37.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "95b5b6a9be4b3e6159775419dc049e62",
+    "url": "/Robofriends/index.html"
+  }
+];
